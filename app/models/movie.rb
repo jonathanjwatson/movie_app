c3ac2a8ae3dee2d62e08c206177beef981fb7160
jwats287 <jwats287@users.noreply.github.com>
2017-08-29 14:50:17 -0400

    Added new migration for api_id

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,18 @@
-class Movie < ApplicationRecord
-end
+class Film < ApplicationRecord
+    include HTTParty
+    base_uri 'http://www.omdbapi.com/?apikey=d31f1a94'
+    &i=tt2294629
+  
+    def self.generate(api_id)
+      film = find_by api_id: api_id
+      return star_war unless star_war.nil?
+  
+      response = get "/#{api_id}"
+      placeholder_image = "https://www.placecage.com/c/#{200 + api_id}/#{200 + api_id}"
+  
+      create!(name: response['name'],
+              image: placeholder_image,
+              wins: 0, 
+              api_id: api_id)
+    end
+  end
---
+++ db/migrate/20170829184916_add_api_id_to_films.rb
@@ -0,0 +1,4 @@
+class AddApiIdToFilms < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
